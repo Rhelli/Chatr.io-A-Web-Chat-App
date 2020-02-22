c969826bfef72c2d6c234fc603d70a6ed11bbcfd
Rory Hellier <rory.hellier@googlemail.com>
2020-02-22 15:33:41 +0000

    Rebuilt rails app due to unavoidable errors in db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_143511) do
+ActiveRecord::Schema.define(version: 2020_02_22_152637) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
